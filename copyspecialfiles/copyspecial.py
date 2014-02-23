a40ce8a4ef2d3d431d20e40a39eb5d6106d681f7
OpenShift guest <thangabalu@gmail.com>
2014-02-23 16:28:20 -0500

    Argument parser

--- copyspecialfiles/copyspecial.py
+++ copyspecialfiles/copyspecial.py
@@ -12,8 +12,17 @@ import os
 import shutil
 import commands
 import zipfile
+import argparse
 
 """Copy Special exercise
+The copyspecial.py program takes one or more directories as its arguments. A "special" file is one where the name contains the pattern __w__ somewhere, 
+where the w is one or more word chars.
+
+Special files will be treated depending on the input given by the user.
+
+usage: copy special names [-h] [--todir TODIR] [--zip ZIP]
+                          inputdirectory [inputdirectory ...]
+
 """
 
 def copyspecialfiles(directoryname):
@@ -29,36 +38,39 @@ def copyspecialfiles(directoryname):
     return absolutepath_list
 
 def main():
-    print 'Usage: [--todir dir] [--tozip zipfile] dir dir dir'
-    directorynames = sys.argv[1:]
-    i = 0
-    todirectory = 'false'
-    tozip = 'false'
-    todirectorypath = ''
-    for arguments in directorynames:
-        if arguments == '--todir':
-            todirectory = 'true'
-            index = i
-        elif arguments == '--tozip':
-            tozip = 'true'
-            index = i
-    #Removing '--todir' from arguments if it is present
-    if todirectory == 'true':
-        todirectorypath = directorynames[index+1]
-        del directorynames[index:index+2]
+    #usage: copy special names [-h] [--todir TODIR] [--zip ZIP]
+    #                          inputdirectory [inputdirectory ...]
+    #--todir and --zip ---> optional arguments
+    #inputdirectory ---> positional arguments(mandatory argument)
+    
+    parser = argparse.ArgumentParser(description = 'Copy the special file names and do the appropriate action according to user input', prog ='copy special names')
+    parser.add_argument('--todir','--todirectory', help = 'Files will be copied to this directory')
+    parser.add_argument('--zip','--tozip', help = 'Copied files will be zipped into this file')
+    parser.add_argument('inputdirectory',nargs='+', help = 'This is the input directory where the special file names will be searched')
+    args = parser.parse_args()
+
+    if args.todir:
+        print 'to directory argument is given - %s' %(args.todir)
+        output_directory = args.todir
+        input_directory = args.inputdirectory
+        print 'input directory argument is %s' %(input_directory)
+    
+    if args.zip:
+        zipfile_name = args.zip
+        input_directory = args.inputdirectory
+        print 'Zip argument is given - %s' %(args.zip)
         
-    #Removing '--tozip' from arguments if it is present
-    if tozip == 'true':
-        zipfile_name = directorynames[index+1]
-        del directorynames[index:index+2]
+    if not args.todir and not args.zip:
+        input_directory = args.inputdirectory
     
     #Pass the directory name to a function and get the special file names.
     copiedfilenames =[]
-    for directoryname in directorynames:
+    for directoryname in input_directory:
         copiedfilenames = copyspecialfiles(directoryname)
+    
     #if '--todir' option is given, write the special files to the given directory.
-        if todirectory == 'true':
-            absolutepath_todirectory = os.path.abspath(todirectorypath)
+        if args.todir:
+            absolutepath_todirectory = os.path.abspath(output_directory)
             if not os.path.exists(absolutepath_todirectory):
                 print 'creating directory'
                 os.mkdir(absolutepath_todirectory)
@@ -68,9 +80,8 @@ def main():
                 print 'copying files to the said directory'
                 shutil.copy(filename,absolutepath_todirectory)
 
-    #if '--tozip' option is given, zip the files under the given zip file name  
-    #TODO : Check if the zipfile already exists. Don't create if it already exists
-        elif tozip == 'true':
+    #if '--zip' option is given, zip the files under the given zip file name  
+        elif args.zip:
             current_working_directory = os.getcwd()
             zipfile_path = os.path.join(current_working_directory,zipfile_name)
             print 'zip file path is %s' %(zipfile_path)
@@ -81,8 +92,9 @@ def main():
             for filename in copiedfilenames:
                 zip_file.write(filename)
         else:
+            #If no option is given, just print it
             print copiedfilenames
-    if tozip == 'true':
+    if args.zip:
         zip_file.close()
 
 if __name__ == "__main__":
