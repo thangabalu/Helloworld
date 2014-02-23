bf26f4cab2b5ca878f8d0065f33e3e10928396d4
OpenShift guest <thangabalu@gmail.com>
2014-02-23 16:30:09 -0500

    minor updates

--- Fetchyoutube/fetch.py
+++ Fetchyoutube/fetch.py
@@ -5,14 +5,69 @@ import termcolor
 
 
 def main():
-    htmlfile = urllib.urlopen('http://www.youtube.com').read()
+    
+    url_parameter = sys.argv[1]
+
+    #write it to a file or html or in any format which user asks
+    # Ask the user whether to start from the youtube.com or particular link in the youtube.com
+    
+    #Usage should be something like
+    #fetch.py -url(link1, link2, link3) -keyword(key1,key2,key3) -length(number) -stop(when the repetition is this number) -to (text file, html file, commandprompt) -wheretostart(homepage, or specific url)
+    
+    
+    #This is to search in www.youtube.com with recommended videos.
+    #video_title = re.findall('<a class=\".*\"\s+ dir=\"ltr\"\s+title=\"(.*)\"\s+data-sessionlink=\".*\"\s+href=\"(.*\")\s+',htmlfile)
+    #for title,url in video_title:
+        #url = '"'+'http://www.youtube.com'+url
+        #if url not in dictionary.values():
+            #dictionary[title] = url
+            #url_list.append(url)
+    
     dictionary ={}
-    print 'started searching'
-    video_title = re.findall('<a class=\".*\"\s+ dir=\"ltr\"\s+title=\"(.*)\"\s+data-sessionlink=\".*\"\s+href=\"(.*)\"\s+',htmlfile)
-    print 'completed searching'
-    for title,url in video_title:
-        url = "http://www.youtube.com"+url
-        dictionary[title] = url
+    url_list =[url_parameter]
+    url_temporary_list=[]
+    url_already_dictionary = 0
+    url_already_dictionary_size = 10000
+    dictionary_length = 3000
+
+    while (len(dictionary) < dictionary_length) and (url_already_dictionary < url_already_dictionary_size):
+        for url in url_list:
+            if url.startswith('"'):
+                url = url[1:]
+            if url.endswith('"'):
+                url = url[:-1]
+            
+            htmlfile = urllib.urlopen(url).read()
+            video_href= re.findall('<li class=\"video-list-item related-list-item\"><a href=\"(.*)\" class',htmlfile)
+            video_title = re.findall('<span dir=\"ltr\" class=\"title\" title=\"(.*\")>',htmlfile)
+    
+            for title, href in zip(video_title,video_href):
+                href = 'http://www.youtube.com'+href
+                
+                if url_already_dictionary > url_already_dictionary_size or len(dictionary) > dictionary_length:
+                    break
+                if href.startswith('"'):
+                    href = href[1:]
+                if href.endswith('"'):
+                    href = href[:-1]
+                if title.startswith('"'):
+                    title = title[1:]
+                if title.endswith('"'):
+                    title = title[:-1]
+                if href in dictionary.values():
+                    url_already_dictionary += 1
+                    print 'url already in dictionary and the total count is %s' %(url_already_dictionary)
+                if href not in dictionary.values():
+                    dictionary[title] = href
+                    url_temporary_list.append(href)
+            if url_already_dictionary > url_already_dictionary_size or len(dictionary) > dictionary_length:
+                break
+        if url_already_dictionary > url_already_dictionary_size or len(dictionary) > dictionary_length:
+            break    
+        
+        #Emptying the list to accomodate the new list
+        del url_list[:]
+        url_list = url_temporary_list
     
     maximum_length_title = max([len(title) for title in dictionary.keys()])
     maximum_length_url = max([len(url) for url in dictionary.values()])
@@ -20,7 +75,10 @@ def main():
     for title,url in dictionary.items():
         url = termcolor.colored(url,'red')
         print title.ljust(maximum_length_title + 2) + url.ljust(maximum_length_url)
-        
+
+    print len(dictionary)
+    
+
 
 if __name__== "__main__":
     main()
--- learning_argparse/learn_argparse.py
+++ learning_argparse/learn_argparse.py
@@ -5,14 +5,14 @@
 
 import sys
 import argparse
+import os
 
 
 """
 Learning Argparse
 """
 
-REPORT_TAGS = ['pass', 'fail', 'error']    
-
+#TODO - Use this function to validate the input
 def dir_arg(arg):
     if os.path.isdir(arg):
         return arg
@@ -21,23 +21,34 @@ def dir_arg(arg):
         raise argparse.ArgumentTypeError(msg)
 
 def main():
-    print 'hello'
+    print 'this is main'
 
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description='Inspects and report on the Python test cases.',
-        prog='testimony')
-    parser.add_argument(
-        'report', type=str, choices=REPORT_TAGS,
-        metavar='REPORT',
-        help='report type, possible values: %s' % ', '.join(REPORT_TAGS))
-    parser.add_argument(
-        'paths', metavar='PATH', type=dir_arg, nargs='+',
-        help='a list of paths to look for tests cases')
+    parser = argparse.ArgumentParser(description='Inspects and report on the Python test cases.',prog='testimony')
+    #Positional arguments (mandatory arguments)
+    parser.add_argument('square', type=int, help='square a number', nargs='+')
+    parser.add_argument('fyra', type=int, help='square a number', nargs='+')
+    parser.add_argument('trice', type=int, nargs='+', help='a list of paths to look for tests cases')
+    
+    #optional argument. Needs one value
+    parser.add_argument('-r','--rank',help ='tell the rank')
+
+    #optional argument. More than one value can be given.
+    parser.add_argument('-f','--friends',type=str, help ='tell the rank', nargs='+')
+    
+    #optional argument with option, action = 'store_true' which stores the value, true or false. Store_true can be used only with optional arguments
     parser.add_argument(
         '-n', '--nocolor', action='store_true', help='Do not use color option')
 
     args = parser.parse_args() 
+    if args.square:
+        print 'you entered the option square'
+        print '%s'%(args.square)
+    if args.trice:
+        print 'you entered the option thrice'
+        print '%s'%(args.trice)
+    if args.friends:
+        print 'Friends - %s' %(args.friends)
     main()
