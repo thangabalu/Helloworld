6bab1ce60d876b754e91aba8ba1373783ba93cba
thangabalu <thangabalu@gmail.com>
2014-03-06 01:58:33 +0100

    Adding virtualenv django project

---
+++ django-prabhu/djangotest/djangotest/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+
+#from django.contrib import admin
+#admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'djangotest.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^hello/$', 'article.views.hello'),
+    url(r'^hello_template/$', 'article.views.hello_template'),
+    url(r'^hello_template_simple/$', 'article.views.hello_template_simple'),
+    # url(r'^admin/', include(admin.site.urls)),
+)
